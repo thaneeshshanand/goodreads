f86d71126bdfb394754b34aabedbe4c35f74c703
Thaneesh Shanand L A <thaneeshshanandla@FWS-CHE-LT-2796.freshworks-corp.com>
2020-08-04 12:57:09 +0530

    cleanup

--- app/helpers/wishlists_helper.rb
+++ app/helpers/wishlists_helper.rb
@@ -11,18 +11,14 @@ module WishlistsHelper
   end
 
   def cache_method_1(key)
-    puts "******** counts called method 1"
     Rails.cache.fetch([key, __method__], expires_in: 2.minutes) do
-      puts "********* NOT CACHED METHOD 1"
       Wishlist.where(user_id: current_user.id).count
     end
   end
 
   def cache_method_2(key)
-    puts "******** counts called method 2"
     count = RedisCache.redis.get(key)
     if count.nil?
-      puts "********* NOT CACHED METHOD 2"
       count = Wishlist.where(user_id: current_user.id).count
       RedisCache.redis.set(key, count)
     end
